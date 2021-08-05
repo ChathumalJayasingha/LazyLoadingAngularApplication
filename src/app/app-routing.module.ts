a158067d1b941c423c7388b846888b93ec62d9ef
Chathumal <chathumaljayasingha@hotmail.com>
2021-08-05 21:28:59 +0530

    create customer module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer', loadChildren: () => import('./dashboard/customer/customer.module').then(m => m.CustomerModule)},
+  // {path:'' ,redirectTo:'/customer', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
